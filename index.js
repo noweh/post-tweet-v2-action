8a6c722ad419a9a362ff652d927e997ba0dcbec3
SCHMITT Julien <j.schmitt@mazarine.com>
2023-09-18 16:59:31 +0200

    update node_modules and test another thing

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const {Client, auth} = require('twitter-api-sdk');
+//const {Client, auth} = require('twitter-api-sdk');
+const { TwitterApi } = require('twitter-api-v2');
 
 try {
     console.log("start to prepare a tweet");
 
-    const authClient = new auth.OAuth2User({
+    const client = new TwitterApi(core.getInput('bearer-token'));
+
+    /*const authClient = new auth.OAuth2User({
         client_id: core.getInput('consumer-key'),
         client_secret: core.getInput('consumer-secret'),
         token: core.getInput('access-token'),
+        callback: "http://127.0.0.1:3000/callback",
+        scopes: ["tweet.write", "offline.access", "tweet.read", "users.read"],
         token_secret: core.getInput('access-token-secret')
     })
-    const client = new Client(authClient);
+    const client = new Client(authClient);*/
 
-    client.tweets.createTweet({
-        text: core.getInput('message')
-    }).then((tweet) => {
+    client.v2.tweet(core.getInput('message')).then((tweet) => {
         console.log(`Tweet posted with ID ${tweet.data.id}`);
 
         const time = (new Date()).toTimeString();
