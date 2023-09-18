42334ec38fbe381006649c9eb35d85a5b7cab58d
SCHMITT Julien <j.schmitt@mazarine.com>
2023-09-18 15:30:28 +0200

    update index.js

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ try {
         const time = (new Date()).toTimeString();
         core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
         console.log(`The event payload: ${payload}`);
     }).catch((error) => {
-        core.setFailed(error.message);;
+        console.log(`An error has occurred: ${error.message}`);
+        core.setFailed(error.message);
     });
 } catch (error) {
+    console.log(error.message);
     core.setFailed(error.message);
 }
