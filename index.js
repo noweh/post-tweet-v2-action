8e79786526ce25d3d6f52862f22ec067c0898caf
SCHMITT Julien <j.schmitt@mazarine.com>
2023-09-18 15:12:23 +0200

    update script index.js

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const {Client} = require('twitter-api-sdk');
 
 try {
     const client = new Client(core.getInput('bearer_token'));
-    const tweet = await client.tweets.createTweet({
+    client.tweets.createTweet({
         text: core.getInput('message')
-    });
-    console.log(`Tweet posted with ID ${tweet.data.id}`);
+    }).then((tweet) => {
+        console.log(`Tweet posted with ID ${tweet.data.id}`);
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    }).catch((error) => {
+        core.setFailed(error.message);;
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
